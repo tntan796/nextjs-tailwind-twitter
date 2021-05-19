aa7d17c1ce149eee52721ef683b0ceddeed38399
Tan Tano <tantn1@sunshinegroup.vn>
2021-05-19 14:21:32 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+    future: {},
+    purge: [],
+    theme: {
+      colors: {
+        body: {
+          default: 'hsl(210, 34.4%, 17.5%)'
+        }
+      },
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  }
+  
